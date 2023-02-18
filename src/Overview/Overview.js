3ab600132389e816ed24c6d1ae2d15f152b75ca9
teeds <teedsthemyth@gmail.com>
2023-02-18 13:41:14 -0700

    Added fade animations to overview

--- src/Overview/Overview.js
+++ src/Overview/Overview.js
@@ -4,7 +4,7 @@ import './Overview.css'
 
 function Language(props) {
     return (
-        <div className="language-wrapper sfpro">
+        <div className="fade language-wrapper sfpro">
             <a>{props.lang}</a>
         </div>
     )
@@ -22,6 +22,57 @@ export class Overview extends React.Component {
             languages: languages, 
             resources: resources
         }
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    /**
+     * Checks if an element is in the viewport
+     * 
+     * @param {*} className - the name of the class to check if it's in the viewport
+     */
+    checkIfInViewport = (className, startAmt, endAmt) => {
+
+        const elements = document.getElementsByClassName(className);
+
+        for (let i = 0; i < elements.length; i++) {
+            const rect = elements[i].getBoundingClientRect();
+
+            const windowHeight = (window.innerHeight || document.documentElement.clientHeight) - 80;
+            const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+
+            const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+            const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+
+            if (vertInView && horInView) {
+                elements[i].style.opacity = "1"
+                elements[i].style.marginTop = `${endAmt}vw`;
+            } else {
+                elements[i].style.opacity = "0"
+                elements[i].style.marginTop = `${startAmt}vw`;
+            }
+        }
+    }
+
+    /**
+     * checks if this component has mounted
+     */
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+        this.handleScroll();
+    }
+
+    /**
+     * checks if the version will be unmomunted
+     */
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    /**
+     * handles if the viewport has been scrolled
+     */
+    handleScroll() {
+        this.checkIfInViewport("overview-skills", "5", "-5");
     }
 
     render() {
@@ -30,22 +81,22 @@ export class Overview extends React.Component {
                 <div className="overview-skills">
                     <div className="overview-section">
                         <div className="ovw-left-wrapper">
-                            <a className="sfproB ovw-name">Theo Kremer</a>
-                            <p className="sfpro ovw-name-subtitle">overview summary</p>
-                            <a className="sfproB ovw-title">Full-Stack Developer</a>
-                            <p className="sfpro ovw-title-description">Through designing user-interfaces, databases, and server side creation I have expterise in developing applications at each layer of the technology stack, front-end and back-end.</p>
+                            <a className="sfproB fade ovw-name">Theo Kremer</a>
+                            <p className="sfpro fade ovw-name-subtitle">overview summary</p>
+                            <a className="sfproB fade ovw-title">Full-Stack Developer</a>
+                            <p className="sfpro fade ovw-title-description">Through designing user-interfaces, databases, and server side creation I have expterise in developing applications at each layer of the technology stack, front-end and back-end.</p>
                         </div>
                         <div className="ovw-right-wrapper">
                             <div className="ovw-skills-wrapper">
-                                <a className="sfproB ovw-pg-lang-title">Programming Languages</a>
-                                <p className="sfpro ovw-pg-lang-subtitle">the various languages I have practice coding and confidence using</p>
+                                <a className="sfproB fade ovw-pg-lang-title">Programming Languages</a>
+                                <p className="sfpro fade ovw-pg-lang-subtitle">the various languages I have practice coding and confidence using</p>
                                 {this.state.languages.map((item, i) => {
                                     return <Language key={i} lang={item}/>
                                 })}
                             </div>
                             <div className="ovw-resources-wrapper">
-                                <a className="sfproB ovw-pg-lang-title">Resources</a>
-                                <p className="sfpro ovw-pg-lang-subtitle">the different tools I use for efficient and effective programming</p>
+                                <a className="sfproB fade ovw-pg-lang-title">Resources</a>
+                                <p className="sfpro fade ovw-pg-lang-subtitle">the different tools I use for efficient and effective programming</p>
                                 {this.state.resources.map((item, i) => {
                                     return <Language key={i} lang={item}/>
                                 })}
