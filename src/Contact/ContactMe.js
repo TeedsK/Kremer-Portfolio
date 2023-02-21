34a942d25238de464c01eb637de7b092a72e94db
teeds <teedsthemyth@gmail.com>
2023-02-21 02:18:16 -0700

    Added repeatable animation for airplane on scroll

--- src/Contact/ContactMe.js
+++ src/Contact/ContactMe.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './ContactMe.css'
+import { gsap } from "gsap";
 
 function ContactInput(props) {
     return (
@@ -41,30 +42,77 @@ export class ContactMe extends React.Component {
                 let object1 = document.getElementById('animated-svg');
                 let svg = object1.contentDocument.getElementById('eAp0ERelTqN1');
                 if(svg != null) {
-                    svg.svgatorPlayer.play();
                     this.setState({
                         animationRan: true
                     })
+                    svg.svgatorPlayer.play();
+                    gsap.to("#cnt-msg-title", {
+                        duration: 0.75,
+                        delay: 0.95,
+                        backgroundPosition: "0% 50%",
+                        ease: "power1.out",
+                    })
+                    gsap.to("#cnt-msg-send-btn", {
+                        duration: 1.0,
+                        delay: 2.9,
+                        width: "14vw",
+                        marginLeft: "2vw",
+                        ease: "power1.out",
+                        onComplete: () => {
+                            gsap.to("#cnt-msg-send-btn", {
+                                duration: 0.5,
+                                ease: "power1.inOut",
+                                width: "12vw",
+                                marginLeft: "0vw",
+                            })
+                        }
+                    })
+                    gsap.to("#cnt-msg-send-btn-text", {
+                        duration: 1.5,
+                        delay: 2.9,
+                        marginLeft: "-1.5vw",
+                        ease: "power1.out",
+                    })
+                    gsap.to("#cnt-btn-arrow-wrapper", {
+                        duration: 0.75,
+                        delay: 2.9,
+                        marginLeft: "8.4vw",
+                        ease: "power1.out",
+                    })
+                    
                 }
             }
             
-            //Wait for the SVGtor player to be ready for use
-            // svg.svgatorPlayer.ready(function (player) {
-            //     player.play();
-
-            //     document.getElementById("cnt-msg-title").style.animationPlayState = "running";
-            //     document.getElementsByClassName('message-send-btn')[0].style.animationPlayState = "running";
-            //     document.getElementsByClassName('msg-send-btn-text')[0].style.animationPlayState = "running";
-            //     document.getElementsByClassName('cnt-btn-arw-wrapper')[0].style.animationPlayState = "running";
-            // });
-
-            // document.getElementById('cnt-msg-title').style.animationPlayState = "running";
-
         } else {
-            // document.getElementById("cnt-msg-title").style.animationPlayState = "paused";
-            // document.getElementsByClassName('message-send-btn')[0].style.animationPlayState = "paused";
-            // document.getElementsByClassName('msg-send-btn-text')[0].style.animationPlayState = "paused";
-            // document.getElementsByClassName('cnt-btn-arw-wrapper')[0].style.animationPlayState = "paused";
+
+            if(this.state.animationRan) {
+                let object1 = document.getElementById('animated-svg');
+                let svg = object1.contentDocument.getElementById('eAp0ERelTqN1');
+                if(svg != null) {
+                    svg.svgatorPlayer.restart();
+                    svg.svgatorPlayer.pause();
+                    gsap.to("#cnt-msg-title", {
+                        duration: 0.3,
+                        backgroundPosition: "100% 50%"
+                    })
+                    gsap.to("#cnt-msg-send-btn", {
+                        duration: 0.3,
+                        width: "12vw",
+                        marginLeft: "0vw"
+                    })
+                    gsap.to("#cnt-msg-send-btn-text", {
+                        duration: 0.3,
+                        marginLeft: "0vw"
+                    })
+                    gsap.to("#cnt-btn-arrow-wrapper", {
+                        duration: 0.3,
+                        marginLeft: "-13vw"
+                    })
+                    this.setState({
+                        animationRan: false
+                    })
+                }
+            }
         }
     }
 
@@ -74,32 +122,6 @@ export class ContactMe extends React.Component {
     componentDidMount() {
         window.addEventListener('scroll', this.handleScroll);
         this.handleScroll();
-
-        let object1 = document.getElementById('animated-svg');
-        object1.addEventListener("load", function () {
-            //grab the <svg> element
-            let svg = object1.contentDocument.getElementById('eAp0ERelTqN1');
-
-            //Wait for the SVGtor player to be ready for use
-            svg.svgatorPlayer.ready(function (player) {
-                player.on('play', () => {
-                    if(!this.state.animationRan) {
-                        document.getElementById("cnt-msg-title").style.animationPlayState = "running";
-                        document.getElementsByClassName('message-send-btn')[0].style.animationPlayState = "running";
-                        document.getElementsByClassName('msg-send-btn-text')[0].style.animationPlayState = "running";
-                        document.getElementsByClassName('cnt-btn-arw-wrapper')[0].style.animationPlayState = "running";
-                    }
-                });
-            });
-        });
-
-
-        // svg.svgatorPlayer.on( 'play', () => {
-        //     document.getElementById("cnt-msg-title").style.animationPlayState = "running";
-        //     document.getElementsByClassName('message-send-btn')[0].style.animationPlayState = "running";
-        //     document.getElementsByClassName('msg-send-btn-text')[0].style.animationPlayState = "running";
-        //     document.getElementsByClassName('cnt-btn-arw-wrapper')[0].style.animationPlayState = "running";
-        // });
     }
 
     /**
@@ -123,7 +145,7 @@ export class ContactMe extends React.Component {
                 <object id="animated-svg" type="image/svg+xml" data="/images/PaperAirplane.svg"></object>
 
                 <div className="contact-me-header">
-                    <a className="sfproSB">Send me a<span id="cnt-msg-title" className="cnt-ani-msg"> Message</span></a>
+                    <a className="sfproSB">Send me a<span id="cnt-msg-title"> Message</span></a>
                     <p className="sfpro">fill out the information below so I know how to reach back to you then press send message and I'll recieve an email</p>
                 </div>
                 {/* <form> */}
@@ -136,9 +158,9 @@ export class ContactMe extends React.Component {
                     <ContactInput width={"55vw"} text="Description" />
                 </div>
                 <div className="cnt-row contact-me-third-row">
-                    <div className="message-send-btn">
-                        <a className="sfpro msg-send-btn-text">send message</a>
-                        <div className="cnt-btn-arw-wrapper">
+                    <div id="cnt-msg-send-btn">
+                        <a id="cnt-msg-send-btn-text" className="sfpro">send message</a>
+                        <div id="cnt-btn-arrow-wrapper">
                             <svg className="exp-ar-ani" id="bgIWRvPa5dV1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300" shapeRendering="geometricPrecision" textRendering="geometricPrecision">
                                 <g className="exp-ar-ani" id="bgIWRvPa5dV2_to" transform="translate(82.819275,150)">
                                     <g className="exp-ar-ani" id="bgIWRvPa5dV2_ts" transform="scale(0.895743,0.895743)">
